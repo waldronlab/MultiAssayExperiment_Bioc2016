729a2772f52f42a8517327794c8893df3d59298a
LiNk-NY <mramos09@gmail.com>
2016-06-25 16:41:59 -0700

    Modify openRmd

--- R/openRmd.R
+++ R/openRmd.R
@@ -4,9 +4,9 @@
 #' 
 #' @export
 openRmd <- function() {
-file.edit(
-    dir(file.path(.libPaths()[1], "MultiAssayExperimentBioc2016",
-                        "doc", "MultiAssayExperimentBioc2016.Rmd"),
-              full.names = TRUE)
+    file.edit(
+        system.file(
+            "doc/MultiAssayExperimentBioc2016.Rmd", 
+            package = "MultiAssayExperimentBioc2016")
     )
 }
